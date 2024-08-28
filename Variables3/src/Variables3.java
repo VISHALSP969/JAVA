580d4c6ee550331341f030727eb9f1174d6bd822
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 21:51:47 +0530

    Variables

---
+++ Variables3/src/Variables3.java
@@ -0,0 +1,20 @@
+
+public class Variables3 {
+
+	public static void main(String[] args) {
+		//Student data
+		String studentName="John Doe";
+		int studentId=15;
+		int studentAge=23;
+		float studentFee=75.25f;
+		char studentgrade='B';
+		
+		//Print variables
+		System.out.println("Student name : "+studentName);	
+		System.out.println("Student id : "+studentId);
+		System.out.println("Student age : "+studentAge);
+		System.out.println("Student fee : "+studentFee);
+		System.out.println("Student grade : "+studentgrade);
+	}
+
+}
