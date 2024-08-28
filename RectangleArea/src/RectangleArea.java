e86a1c728cd9104591a234c8b0314fee3ac69c79
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 22:11:57 +0530

    Area of rectangle

---
+++ RectangleArea/src/RectangleArea.java
@@ -0,0 +1,19 @@
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		//create integer variables
+		int length=4;
+		int width=6;
+		int area;
+		
+		//calculate the area of a triangle
+		area=length*width;
+		
+		//print variables
+		System.out.println("Length is : "+length);
+		System.out.println("Width is : "+width);
+		System.out.println("Area of the rectangle is : "+area);
+	}
+
+}
