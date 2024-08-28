864ddd5584ee6024d41e521cb5796006c7bac83b
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 18:29:18 +0530

    Comments

---
+++ Comments/src/Comments.java
@@ -0,0 +1,14 @@
+
+public class Comments {
+
+	public static void main(String[] args) {
+		//This is a single line comment
+		System.out.println("Hello world...!");
+		
+		/*This is a 
+		 multi line comment*/
+		System.out.println("Welcome to JAVA.");
+
+	}
+
+}
