546c45ce01676c6a6843ca19438e3a30dd6ad101
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 23:46:28 +0530

    Datatypes

---
+++ Numbers/src/Numbers.java
@@ -0,0 +1,32 @@
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		//Integer types
+		byte myNum1=100;
+		System.out.println(myNum1);
+		
+		short myNum2=5000;
+		System.out.println(myNum2);
+		
+		int myNum3=100000;
+		System.out.println(myNum3);
+		
+		long myNum4=15000000000L;
+		System.out.println(myNum4);
+		
+		//floating point types
+		float myNum5=5.75f;
+		System.out.println(myNum5);
+		
+		double myNum6=19.99d;
+		System.out.println(myNum6);
+		
+		//scientific numbers
+		float f1=35e3f;
+		double d1=12E4d;
+		System.out.println(f1);
+		System.out.println(d1);
+	}
+
+}
