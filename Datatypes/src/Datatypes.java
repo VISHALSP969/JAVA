ef06636211c5ec48fb485949e64bc6f295209f58
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 23:17:11 +0530

    Datatypes

---
+++ Datatypes/src/Datatypes.java
@@ -0,0 +1,18 @@
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int myNum=5;		//integer
+		float myFloatNum=5.99f;		//floating point number
+		char myLetter='D';			//character
+		String myText="Hello";		//string
+		boolean myBool=true;		//boolean
+		
+		System.out.println(myNum);
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myText);
+		System.out.println(myBool);
+	}
+
+}
