91c2aeecb431965dae0f6ea5fd683498bf803e50
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 17:14:30 +0530

    Print function

---
+++ PrintFunction/src/PrintFunction.java
@@ -0,0 +1,10 @@
+
+public class PrintFunction {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I am learning JAVA.");
+		System.out.println("It is awsome!");
+	}
+
+}
