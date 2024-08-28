4186977a8f0d4534349dc2816a6eecc4210c0e9f
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 18:23:42 +0530

    Print function

--- PrintFunction/src/PrintFunction.java
+++ PrintFunction/src/PrintFunction.java
@@ -5,6 +5,23 @@ public class PrintFunction {
 		System.out.println("Hello World!");
 		System.out.println("I am learning JAVA.");
 		System.out.println("It is awsome!");
+		System.out.println("---------------------");
+		
+		//for print() , no line break
+		System.out.print("Hello World!");
+		System.out.print("I will print on the same line.");
+		System.out.println("\n---------------------");
+		
+		System.out.println("Print numbers");
+		System.out.println(3);
+		System.out.println(358);
+		System.out.println(50000);
+		System.out.println("---------------------");
+		
+		//expression inside println function
+		System.out.println(3+3);
+		System.out.println(2*5);
+		System.out.println("---------------------");
 	}
 
 }
