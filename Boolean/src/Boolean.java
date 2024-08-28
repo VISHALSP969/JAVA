0632b397562491bd7e2aaa8c37af51dc675b9ae8
VISHALSP969 <vishalsp969@gmail.com>
2024-08-28 23:50:40 +0530

    Datatypes

---
+++ Boolean/src/Boolean.java
@@ -0,0 +1,11 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		boolean isJavaFun=true;
+		boolean isFishTasy=false;
+		System.out.println(isJavaFun);
+		System.out.println(isFishTasy);
+	}
+
+}
