81eda96058a572dd9e09d18ac2cd24ae5ba85261
VISHALSP969 <vishalsp969@gmail.com>
2024-08-29 09:46:04 +0530

    Strings

--- Strings1/src/Strings1.java
+++ Strings1/src/Strings1.java
@@ -24,6 +24,43 @@ public class Strings1 {
 	    System.out.println(firstName + " " + lastName);
 	    //using concat() method
 	    System.out.println(firstName.concat(lastName));
+	
+	    int x = 10;
+	    int y = 20;
+	    int z = x + y;
+	    System.out.println(z);
+	    
+	    String a = "10";
+	    String b = "20";
+	    String c = a + b;
+	    System.out.println(c);
+	    
+	    String p = "10";
+	    int q = 20;
+	    String r = p + q;
+	    System.out.println(r);
+	    
+	    //special characters
+	    txt = "We are the so-called \"Vikings\" from the north.";
+	    System.out.println(txt);
+	    
+	    txt = "It\'s alright.";
+	    System.out.println(txt);
+	    
+	    txt = "The character \\ is called backslash.";
+	    System.out.println(txt);
+	    
+	    txt = "Hello\nWorld!";
+	    System.out.println(txt);
+	    
+	    txt = "Hello\rWorld!";
+	    System.out.println(txt);
+	    
+	    txt = "Hello\tWorld!";
+	    System.out.println(txt);
+	    
+	    txt = "Hel\blo World!";
+	    System.out.println(txt);	    
 	}
 
 }
