9650db7c028026fea020b4566bf92c3bcbd3d6e7
VISHALSP969 <vishalsp969@gmail.com>
2024-08-29 09:30:16 +0530

    Operators

---
+++ LogicalOperators/src/LogicalOperators.java
@@ -0,0 +1,11 @@
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		int x = 5;
+	    System.out.println(x > 3 && x < 10);
+	    System.out.println(x > 3 || x < 4);
+	    System.out.println(!(x > 3 && x < 10));
+	}
+
+}
