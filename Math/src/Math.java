bef159f001bdf965bfafeb6b2c399b0600398386
VISHALSP969 <vishalsp969@gmail.com>
2024-08-29 10:09:17 +0530

    Math

---
+++ Math/src/Math.java
@@ -0,0 +1,14 @@
+
+public class Math {
+
+	public static void main(String[] args) {
+		System.out.println(java.lang.Math.max(5, 10));
+		System.out.println(java.lang.Math.min(5, 10));
+		System.out.println(java.lang.Math.sqrt(64));
+		System.out.println(java.lang.Math.abs(-4.7));
+		
+		//Random numbers
+		System.out.println(java.lang.Math.random());
+		System.out.println(java.lang.Math.random()*101);
+	}
+}
