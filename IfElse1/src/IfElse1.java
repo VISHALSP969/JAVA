0b15699ed46d44adf5fbed7152825d0168bd83db
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 09:34:14 +0530

    Do-While loop

--- IfElse1/src/IfElse1.java
+++ IfElse1/src/IfElse1.java
@@ -2,8 +2,16 @@
 public class IfElse1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int time = 20;
+		if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+		
+		//conditional operator
+		String result = (time < 18) ? "Good day." : "Good evening.";
+		System.out.println(result);
 	}
 
 }
