86e0928917ff5a58d2f26b7e7341ada942368983
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 10:28:11 +0530

    For loop

--- ForLoop/src/ForLoop.java
+++ ForLoop/src/ForLoop.java
@@ -10,6 +10,24 @@ public class ForLoop {
 		for(int i=0;i<10;i++) {
 			System.out.println(i);
 		}
+		System.out.println("---------------");
+		
+		for (int i = 0; i <= 100; i += 10) {
+			System.out.println(i);
+		} 
+		System.out.println("---------------");
+		
+		for (int i = 0; i <= 10; i = i + 2) {
+			System.out.println(i);
+		}
+		System.out.println("---------------");
+		
+		int number = 2;
+		// Print the multiplication table for the number 2
+		for (int i = 1; i <= 10; i++) {
+		  System.out.println(number + " x " + i + " = " + (number * i));
+		} 
+		System.out.println("---------------");
 	}
 
 }
