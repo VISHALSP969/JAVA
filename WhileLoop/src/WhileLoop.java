4ebe3e31d753a2087d6f78177c86ee806bea7392
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 10:11:48 +0530

    While loop

--- WhileLoop/src/WhileLoop.java
+++ WhileLoop/src/WhileLoop.java
@@ -7,6 +7,26 @@ public class WhileLoop {
 			System.out.println(i);
 			i++;
 		}
+		System.out.println("---------------");
+		
+		int countDown=3;
+		while(countDown>0) {
+			System.out.println(countDown);
+			countDown--;
+		}
+		System.out.println("Happy New Year!!");
+		System.out.println("---------------");
+		
+		int dice=1;
+		while(dice<=6) {
+			if(dice<6) {
+				System.out.println("No Yatzy.");
+			} else {
+			System.out.println("Yatzy!");
+			}
+			dice=dice+1;
+		}
+		
 	}
 
 }
