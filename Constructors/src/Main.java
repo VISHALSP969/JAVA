d1cbc9974022827c99295d16a5c913e53e2ad2af
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 21:39:16 +0530

    Constructors

---
+++ Constructors/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	int x;
+	
+	//Create class constructor for main class
+	public Main() {
+		x=5;
+	}
+	
+	public static void main(String[] args) {
+		Main myObj=new Main();
+		System.out.println(myObj.x);
+	}
+}
