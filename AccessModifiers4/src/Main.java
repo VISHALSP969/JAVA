b61886ea0c47e8ac6d5f25ab313bcb74b6a72ecb
VISHALSP969 <vishalsp969@gmail.com>
2024-08-31 02:40:53 +0530

    Default

---
+++ AccessModifiers4/src/Main.java
@@ -0,0 +1,13 @@
+
+class Main {
+	String fname="John";
+	String lname="Doe";
+	String email="john@doe.com";
+	int age=24;
+	public static void main(String[] args) {
+		Main myObj=new Main();
+		System.out.println("Name : "+myObj.fname+" "+myObj.lname);
+		System.out.println("Email : "+myObj.email);
+		System.out.println("Age : "+myObj.age);
+	}
+}
