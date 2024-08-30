d68c72226f8c54143ceff337cfcc64831ca4abb6
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 11:54:50 +0530

    Arrays

---
+++ Arrays2/src/Arrays2.java
@@ -0,0 +1,18 @@
+
+public class Arrays2 {
+
+	public static void main(String[] args) {
+		int[] ages= {20,22,18,35,48,26,87,70};
+		
+		int lowestAge=ages[0];
+		
+		for(int age:ages) {
+			if(lowestAge>age) {
+				lowestAge=age;
+			}
+		}
+		
+		System.out.println("The lowest age in the array is : "+lowestAge);
+	}
+
+}
