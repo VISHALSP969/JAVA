8cc6a60daa03a1aecacea47df9539d037d1ba800
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 13:51:53 +0530

    Method parameters

---
+++ Methods3/src/Methods3.java
@@ -0,0 +1,13 @@
+
+public class Methods3 {
+	
+	static void myMethod(String fname) {
+		System.out.println(fname+" Refsnes");
+	}
+	
+	public static void main(String[] args) {
+		myMethod("Liam");
+		myMethod("Jenny");
+		myMethod("Anja");
+	}
+}
