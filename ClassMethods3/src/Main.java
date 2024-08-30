32d9ffeff1daa5c8ff592aef44d005121f6e488e
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 20:31:03 +0530

    Class methods

---
+++ ClassMethods3/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyClass myObj=new MyClass();
+		myObj.fullThrottle();
+		myObj.speed(200);
+	}
+}
---
+++ ClassMethods3/src/MyClass.java
@@ -0,0 +1,10 @@
+
+public class MyClass {
+	public void fullThrottle() {
+		System.out.println("The car is going as fast as it can!");
+	}
+	
+	public void speed(int maxSpeed) {
+		System.out.println("Max speed is : "+maxSpeed);
+	}
+}
