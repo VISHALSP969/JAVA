6f99fbc541559e801046aa0a4d6eb5a595bc70f6
VISHALSP969 <vishalsp969@gmail.com>
2024-08-30 13:39:36 +0530

    Methods

---
+++ Methods1/src/Methods1.java
@@ -0,0 +1,13 @@
+
+public class Methods1 {
+	
+	static void mymethod() {
+		System.out.println("I just got executed!");
+	}
+
+	public static void main(String[] args) {
+		mymethod();
+		mymethod();
+		mymethod();
+	}
+}
