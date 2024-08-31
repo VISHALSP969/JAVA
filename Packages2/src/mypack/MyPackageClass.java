e7545ec1b713795035bbfac5afac52e356a02d68
VISHALSP969 <vishalsp969@gmail.com>
2024-08-31 10:51:45 +0530

    User defined packages

---
+++ Packages2/src/mypack/MyPackageClass.java
@@ -0,0 +1,11 @@
+package mypack;
+import mypack1.MyClass;
+
+public class MyPackageClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is my package!");
+		MyClass myObj=new MyClass();
+		System.out.println(myObj.x);
+	}
+}
