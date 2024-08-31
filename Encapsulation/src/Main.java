6ab21e8b3dd48ba614134bfac068dee0821cf227
VISHALSP969 <vishalsp969@gmail.com>
2024-08-31 10:39:34 +0530

    Encapsulation

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyClass myObj=new MyClass();
+		myObj.setName("John");
+		System.out.println(myObj.getName());
+	}
+}
---
+++ Encapsulation/src/MyClass.java
@@ -0,0 +1,14 @@
+
+public class MyClass {
+	private String name;
+	
+	//Getter
+	public String getName() {
+		return this.name;
+	}
+	
+	//Setter
+	public void setName(String newName) {
+		this.name=newName;
+	}
+}
