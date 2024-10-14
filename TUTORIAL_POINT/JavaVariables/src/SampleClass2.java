ff96a91ec72d036742c1a92eea47c33938f5db8e
VISHALSP969 <vishalsp969@gmail.com>
2024-10-14 20:59:43 +0530

    LOCAL VARIABLE

---
+++ TUTORIAL_POINT/JavaVariables/src/SampleClass2.java
@@ -0,0 +1,14 @@
+
+public class SampleClass2 {
+
+	public void pupAge() {
+		int age=0;
+		age=age+7;
+		System.out.println("Puppy age is : "+age);
+	}
+	
+	public static void main(String[] args) {
+		SampleClass2 test=new SampleClass2();
+		test.pupAge();
+	}
+}
