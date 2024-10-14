bbafc208b34b6c5b29f5250ac0c52ac514fb56f5
VISHALSP969 <vishalsp969@gmail.com>
2024-10-14 20:53:38 +0530

    JAVA VARIABLES

--- TUTORIAL_POINT/JavaVariables/src/SampleClass1.java
+++ TUTORIAL_POINT/JavaVariables/src/SampleClass1.java
@@ -2,8 +2,13 @@
 public class SampleClass1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a,b,c;		// declare 3 ints
+		int x=10,y=20;	// variable initialization
+		
+		byte B=22;		// initialize a byte type variable
+		
+		double pi=3.14159;	// initialize pi
+		
+		char ch='a';		//initialize char variable	
 	}
-
 }
