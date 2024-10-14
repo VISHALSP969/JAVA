3ec007373d8f11c7b2adb416dcccdc562ef4fcc8
VISHALSP969 <vishalsp969@gmail.com>
2024-10-14 22:52:42 +0530

    CLASS VARIABLES

---
+++ TUTORIAL_POINT/JavaVariables/src/SampleClass4.java
@@ -0,0 +1,14 @@
+
+public class SampleClass4 {
+	
+	// slary variable is a private static variable
+	private static double salary;
+	
+	// DEPARTMENT is a constant
+	public static final String DEPARTMENT="Development";
+	
+	public static void main(String[] args) {
+		salary=35000;
+		System.out.println(DEPARTMENT+" average salary : "+salary);
+	}
+}
