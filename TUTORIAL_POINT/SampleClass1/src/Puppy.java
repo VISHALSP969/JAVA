3b70ae1abbd9b8e4b655b585d5520a7ec89de6ef
VISHALSP969 <vishalsp969@gmail.com>
2024-10-14 14:39:32 +0530

    CLASS METHODS AND INSTANCE VARIABLES

--- TUTORIAL_POINT/SampleClass1/src/Puppy.java
+++ TUTORIAL_POINT/SampleClass1/src/Puppy.java
@@ -1,13 +1,33 @@
 
 public class Puppy {
 	
+	int puppyAge;
+	
 	public Puppy(String name) {
 		// This constructor has one parameter, name
 		System.out.println("Passed name is :"+name);
+	}
+	
+	public void setAge(int age) {
+		puppyAge=age;
+	}
+	
+	public int getAge() {
+		System.out.println("Puppy's age is : "+puppyAge);
+		return puppyAge;
 	}
 
 	public static void main(String[] args) {
 		//Following statement would create an object myPuppy
 		Puppy myPuppy=new Puppy("Tommy");
+		
+		// Call class method to set puppy's age
+		myPuppy.setAge(2);
+		
+		// Call Another class method to get puppy's age
+		myPuppy.getAge();
+		
+		// You can access instance variable as follows as well
+		System.out.println("Variable vale : "+myPuppy.puppyAge);
 	}
 }
