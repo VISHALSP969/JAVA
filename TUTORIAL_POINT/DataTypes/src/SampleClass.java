e997f3915d5fa45be6fb0b9129ef3b2df9e0700e
VISHALSP969 <vishalsp969@gmail.com>
2024-10-14 18:44:46 +0530

    PRIMITIVE DATATYPES

---
+++ TUTORIAL_POINT/DataTypes/src/SampleClass.java
@@ -0,0 +1,27 @@
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// primitive datatypes
+		byte b=15;
+		short s=20;
+		int i=25;
+		long l=30;
+		
+		float f=2.5f;
+		double d=3.5d;
+		
+		boolean bool=false;
+		
+		char ch='A';
+		
+		System.out.println("b : "+b);
+		System.out.println("s : "+s);
+		System.out.println("i : "+i);
+		System.out.println("l : "+l);
+		System.out.println("f : "+f);
+		System.out.println("d : "+d);
+		System.out.println("bool : "+bool);
+		System.out.println("ch : "+ch);
+	}
+}
