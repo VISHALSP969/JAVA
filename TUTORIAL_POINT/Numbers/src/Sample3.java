e36a2c26f8852a722024c4a6abb134954fd2151b
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 23:59:38 +0530

    xxxValue() METHOD

---
+++ TUTORIAL_POINT/Numbers/src/Sample3.java
@@ -0,0 +1,13 @@
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		Integer x=5;
+		System.out.println(x.byteValue());
+		System.out.println(x.shortValue());
+		System.out.println(x.intValue());
+		System.out.println(x.longValue());
+		System.out.println(x.floatValue());
+		System.out.println(x.doubleValue());		
+	}
+}
