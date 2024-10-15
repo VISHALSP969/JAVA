9cbca01b83959de91857b088b61001b52582033b
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 11:23:46 +0530

    FINAL KEYWORD

---
+++ TUTORIAL_POINT/JavaModifiers/src/Test.java
@@ -0,0 +1,23 @@
+// final class
+final class TestClass{
+	//body of class
+	int x=10;
+}
+
+public class Test {
+	
+	// final variable
+	final int value=10;
+	// The following are examples of declaaring constants
+	public static final int BOXWIDTH=6;
+	static final String TITLE="Manager";
+	
+//	public void changeValue() {
+//		value=12;	//will give an error
+//	}
+	
+	// final method
+	public final void changeName() {
+		// body of method
+	}
+}
