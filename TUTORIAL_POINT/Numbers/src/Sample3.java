3ba909bb425228c99041da31167ffee09948123f
VISHALSP969 <vishalsp969@gmail.com>
2024-10-16 00:20:39 +0530

    compareTo() METHOD

--- TUTORIAL_POINT/Numbers/src/Sample3.java
+++ TUTORIAL_POINT/Numbers/src/Sample3.java
@@ -8,6 +8,13 @@ public class Sample3 {
 		System.out.println(x.intValue());
 		System.out.println(x.longValue());
 		System.out.println(x.floatValue());
-		System.out.println(x.doubleValue());		
+		System.out.println(x.doubleValue());
+		System.out.println("-----------");
+		System.out.println(x.compareTo(3));	
+		System.out.println(x.compareTo(4));
+		System.out.println(x.compareTo(5));
+		System.out.println(x.compareTo(6));
+		System.out.println(x.compareTo(7));
+		System.out.println("-----------");
 	}
 }
