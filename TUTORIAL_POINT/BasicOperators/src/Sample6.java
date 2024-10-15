26b8fb6b737f302dfae0adf50ab50a38fc545f06
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 15:12:49 +0530

    TERNARY OPERATOR

---
+++ TUTORIAL_POINT/BasicOperators/src/Sample6.java
@@ -0,0 +1,15 @@
+
+public class Sample6 {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=10;
+		b=(a==1)?20:30;
+		
+		System.out.println("Value of b is "+b);
+		
+		b=(a==10)?20:30;
+		
+		System.out.println("Value of b is "+b);
+	}
+}
