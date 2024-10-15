f2544efd37404099c2fb0f948438e706ff2a029a
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 18:47:32 +0530

    ENHANCED FOR LOOP

---
+++ TUTORIAL_POINT/Loops/src/Sample6.java
@@ -0,0 +1,18 @@
+
+public class Sample6 {
+
+	public static void main(String[] args) {
+		int[] numbers= {10,20,30,40,50};
+		
+		for(int x:numbers) {
+			System.out.println(x);
+		}
+		System.out.println();
+		
+		String[] names= {"James","Larry","Tom","Lacy"};
+		
+		for(String name:names) {
+			System.out.println(name);
+		}
+	}
+}
