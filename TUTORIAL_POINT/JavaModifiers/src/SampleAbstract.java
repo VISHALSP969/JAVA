bf33539e4589dbcbf250c736e48e02552a69e12f
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 12:34:01 +0530

    ABSTRACT KEYWORD

---
+++ TUTORIAL_POINT/JavaModifiers/src/SampleAbstract.java
@@ -0,0 +1,8 @@
+
+abstract class SampleAbstract {
+	private double price;
+	private String model;
+	private String year;
+	public abstract void gofast();	// an abstract method
+	public abstract void changeColor();
+}
---
+++ TUTORIAL_POINT/JavaModifiers/src/SampleAbstract1.java
@@ -0,0 +1,11 @@
+abstract class SuperClass{
+	abstract void m();		// abstract method
+}
+
+class SampleAbstract1 extends SuperClass {
+
+	// implements the abstract method
+	void m() {
+		
+	}
+}
