73a426826b19d8ae851b9b2bab3cee5e81751ef3
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 20:43:16 +0530

    Numbers primitive types

---
+++ TUTORIAL_POINT/Numbers/src/Sample1.java
@@ -0,0 +1,10 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		int i=5000;
+		float gpa=13.65f;
+		byte mask=(byte) 0xaf;
+		System.out.println(i+"  "+gpa+"  "+mask);
+	}
+}
