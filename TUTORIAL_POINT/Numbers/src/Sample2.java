9d106b92d577268dee99a7dec249fe858119a8ef
VISHALSP969 <vishalsp969@gmail.com>
2024-10-15 20:59:40 +0530

    BOXING AND UNBOXING

---
+++ TUTORIAL_POINT/Numbers/src/Sample2.java
@@ -0,0 +1,9 @@
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		Integer x=5;	// boxes int to an Integer object
+		x=x+10;			// unboxex the Integer to a int
+		System.out.println(x);
+	}
+}
