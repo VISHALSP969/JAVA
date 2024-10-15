beb81c169475832206216f42e550262087f11130
VISHALSP969 <vishalsp969@gmail.com>
2024-10-16 03:42:11 +0530

    max() METHOD

--- TUTORIAL_POINT/Numbers/src/Sample3.java
+++ TUTORIAL_POINT/Numbers/src/Sample3.java
@@ -61,5 +61,8 @@ public class Sample3 {
 		System.out.println(Math.min(12.123,12.456));
 		System.out.println(Math.min(23.12,23.0));
 		System.out.println("-----------");
+		System.out.println(Math.max(12.123,12.456));
+		System.out.println(Math.max(23.12,23.0));
+		System.out.println("-----------");
 	}
 }
