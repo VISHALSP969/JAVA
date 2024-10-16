288aaf85d0482c4da3e7f0e0f99e2bba0d1416e2
VISHALSP969 <vishalsp969@gmail.com>
2024-10-16 12:09:30 +0530

    STRING CONCATENATION

---
+++ TUTORIAL_POINT/Strings/src/Sample5.java
@@ -0,0 +1,11 @@
+
+public class Sample5 {
+
+	public static void main(String[] args) {
+		String str1="saw I was";
+		System.out.println("Dot "+str1+" Tod");
+		
+		String str2="Hello";
+		System.out.println(str2.concat(" World"));
+	}
+}
