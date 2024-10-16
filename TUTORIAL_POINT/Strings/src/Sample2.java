cccf80d30e69ae99eb426319feaca4d5d99e1678
VISHALSP969 <vishalsp969@gmail.com>
2024-10-16 10:55:19 +0530

    STRING BUFFER

---
+++ TUTORIAL_POINT/Strings/src/Sample2.java
@@ -0,0 +1,9 @@
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		StringBuffer sBuffer=new StringBuffer(" test");
+		sBuffer.append(" StringBuffer");
+		System.out.println(sBuffer);
+	}
+}
