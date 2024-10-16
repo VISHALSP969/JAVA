9662e5d4119bb889e47571a944d1f893b21bdb1f
VISHALSP969 <vishalsp969@gmail.com>
2024-10-16 09:57:22 +0530

    ESCAPE CHARACTERS

---
+++ TUTORIAL_POINT/Character/src/Sample3.java
@@ -0,0 +1,10 @@
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		System.out.println("She said \"Hell\" to me");
+		System.out.println("Two\nlines");
+		System.out.println("Helloo\b World");
+		System.out.println("First\rSecond");
+	}
+}
