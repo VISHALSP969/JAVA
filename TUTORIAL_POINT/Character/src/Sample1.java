01eaab82654cbbb4cfd5c0fc5d0d625548118f09
VISHALSP969 <vishalsp969@gmail.com>
2024-10-16 09:36:41 +0530

    char PRIMITIVE DATATYPE

---
+++ TUTORIAL_POINT/Character/src/Sample1.java
@@ -0,0 +1,13 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		char ch='a';
+		
+		// unicode for uppercase Greek omega character
+		char uniChar='\u039A';
+		
+		// an array of chars
+		char[] charArray= {'a','b','c','d','e'};
+	}
+}
